7014b8692f108c5185862b38543f417674097fd3
Vinissius Vioti <vinivioti@yahoo.com.br>
2021-11-06 09:30:31 -0300

    primeiro commit

--- providers.tf
+++ providers.tf
@@ -10,8 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/gcpterraformkey.json")
-
+ 
   project = "lab-devops-cloud-330320"
   region  = "us-west1"
   zone    = "us-west1-b"
